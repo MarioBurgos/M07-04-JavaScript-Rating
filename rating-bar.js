7c391548c8027d5491d2f15f32b44e0c31e0f60a
Mario Burgos <mario.burpi@gmail.com>
2021-10-16 18:36:11 +0200

    pinta la estrella objeto del click y todas las posteriores, pero aún no las desactiva

--- rating-bar.js
+++ rating-bar.js
@@ -1,13 +1,15 @@
-const stars = document.getElementById("rating__star");
+const stars = [...document.getElementsByClassName("rating__star")];
 
 const stateActive = 'rating__star fas fa-star fa-2x';
 const stateInactive = 'rating__star far fa-star fa-2x';
 
-function toggleStar(star) {
-    if (star.className === 'rating__star fas fa-star fa-2x') {
-        star.className = 'rating__star far fa-star fa-2x';
-    } else {
-        star.className = 'rating__star fas fa-star fa-2x'
+function toggleStars(star) {
+
+    for (let i = 0; i < star.id; i++) {
+        const s = stars[i];
+        s.className = stateActive;
     }
+
+
 
 }
