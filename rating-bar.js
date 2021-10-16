22be2bf9e93cbb25b5d1ef7eddcb3cd0d2f7d43b
Mario Burgos <mario.burpi@gmail.com>
2021-10-16 18:54:47 +0200

    hace su función.  Valoración: 5 estrellas

--- rating-bar.js
+++ rating-bar.js
@@ -5,6 +5,17 @@ const stateInactive = 'rating__star far fa-star fa-2x';
 
 function toggleStars(star) {
 
+    //controla las estrellas posteriores
+    if (star.className == stateActive) {
+        star.className = stateInactive;
+        stars.forEach(s => {
+            if (s.id > star.id) {
+                s.className = stateInactive;
+            }
+        });
+    }
+
+    //controla las anteriores
     for (let i = 0; i < star.id; i++) {
         const s = stars[i];
         s.className = stateActive;
